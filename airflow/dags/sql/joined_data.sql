7aa1b1d5d2e408be0966e3a06975db5a9cc264a7
Karl Sie <karlchristian11.kc@gmail.com>
2026-03-12 07:40:21 +0400

    refactor

--- airflow/dags/sql/joined_data.sql
+++ airflow/dags/sql/joined_data.sql
@@ -1,3 +1,3 @@
 SELECT kks.*, sh.kategori_sektor, sh.nama_csirt
-FROM public.kinerja_keamanan_siber kks
-LEFT JOIN public.stakeholder sh ON kks.id_stakeholder = sh.id_stakeholder
+FROM public.kinerja_keamanan_siber_dst kks
+LEFT JOIN public.stakeholder_dst sh ON kks.id_stakeholder = sh.id_stakeholder
